53d10c0309c05d505d841498acc1bb962b12e513
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-18 10:06:26 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+from .nodes import LoadCobraModel, ExtractLineArt, GetColorValue, DrawColorHint, ColorizeImage
 
+NODE_CLASS_MAPPINGS = {
+    "LoadCobraModel": LoadCobraModel,
+    "ExtractLineArt": ExtractLineArt,
+    "GetColorValue": GetColorValue,
+    "DrawColorHint": DrawColorHint,
+    "ColorizeImage": ColorizeImage,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadCobraModel": "Load Cobra Model",
+    "ExtractLineArt": "Extract Line Art",
+    "GetColorValue": "Get Color Value",
+    "DrawColorHint": "Draw Color Hint",
+    "ColorizeImage": "Colorize Image",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
